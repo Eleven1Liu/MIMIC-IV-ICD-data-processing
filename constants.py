f3d009c1373d9662abee4d45ff141d0860eae5ba
Eleven Liu <jiejyunliu@gmail.com>
2024-03-17 23:49:48 +0800

    Add constants.py & requirements.txt

---
+++ constants.py
@@ -0,0 +1,13 @@
+PAD_CHAR = "**PAD**"
+EMBEDDING_SIZE = 100
+MAX_LENGTH = 2500
+
+# Path to CAML-MIMIC repo
+REPO_DIR = '/home/eleven/dev/MIMIC-IV-ICD-data-processing'
+
+#where you want to save any models you may train
+MODEL_DIR = f'{REPO_DIR}/saved_models/'
+
+DATA_DIR = f'{REPO_DIR}/mimicdata/'
+MIMIC_3_DIR = f'{REPO_DIR}/mimicdata/mimic3'
+MIMIC_2_DIR = f'{REPO_DIR}/mimicdata/mimic2'
