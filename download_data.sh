94ccb8a834bb255e0346cd5152c076765443e86a
Eleven Liu <jiejyunliu@gmail.com>
2024-03-18 09:14:57 +0800

    Correct file

--- download_data.sh
+++ download_data.sh
@@ -13,12 +13,7 @@ stty echo # enable bash output
 cd mimicdata
 wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciv/2.2/hosp/procedures_icd.csv.gz
 wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciv/2.2/hosp/diagnoses_icd.csv.gz
-wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciii/1.4/NOTEEVENTS.csv.gz
+wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimic-iv-note/2.2/note/discharge.csv.gz
+# wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciii/1.4/NOTEEVENTS.csv.gz
 
-gzip -d ${MIMIC_3_DIR}/*.gz
 gzip -d ${MIMIC_4_DIR}/*/*.gz
-
-# Move note event to `MIMIC_4_dir/note`
-mkdir -p ${MIMIC_4_DIR}/note
-mv ${MIMIC_3_DIR}/NOTEEVENTS.csv ${MIMIC_4_DIR}/note/discharge.csv
-echo "Move ${MIMIC_3_DIR}/NOTEEVENTS.csv to ${MIMIC_4_DIR}/note/discharge.csv"
