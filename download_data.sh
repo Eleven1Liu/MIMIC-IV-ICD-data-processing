a96e73b0f7204e0e45a328a02739daac386fbc5a
Eleven Liu <jiejyunliu@gmail.com>
2024-03-18 11:53:12 +0800

    Update script

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,7 @@
 MIMIC_DATA_DIR='./mimicdata/physionet.org/files'
 MIMIC_4_DIR=${MIMIC_DATA_DIR}/mimiciv/2.2
 # MIMIC_3_DIR=${MIMIC_DATA_DIR}/mimiciii/1.4
+FILES=("mimiciv/2.2/hosp/procedures_icd.csv" "mimiciv/2.2/hosp/diagnoses_icd.csv" "mimic-iv-note/2.2/note/discharge.csv")
 
 USERNAME=$1
 stty -echo # disable bash output
@@ -11,13 +12,16 @@ read passwd
 stty echo # enable bash output
 
 # Download and unzip MIMIC data
-cd mimicdata
-wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciv/2.2/hosp/procedures_icd.csv.gz
-wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciv/2.2/hosp/diagnoses_icd.csv.gz
-wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimic-iv-note/2.2/note/discharge.csv.gz
-# wget -r -N -c -np --user $USERNAME --password $passwd https://physionet.org/files/mimiciii/1.4/NOTEEVENTS.csv.gz
+for file in "${FILES[@]}"; do
+    if [ -f "${MIMIC_DATA_DIR}/$file" ]; then
+        echo "$file exists. Skip"
+    else
+        echo "$file does not exist. Download from physionet."
+        wget -r -N -c -np --user $USERNAME --password $passwd -P mimicdata https://physionet.org/files/$file.gz
+    fi
+done
 
-no | gzip -d -r ${MIMIC_DATA_DIR}
+find ./mimicdata/physionet.org/files/ -name "*.gz" -type f -exec gzip -d {} +
 
 # Move discharge summary to `MIMIC_4_DIR`/note
 mkdir -p ${MIMIC_4_DIR}/note
